5a09d6b95f5675c0157d5341ddc1009b243cd4fc
Dheerajdoppalapudi <dheerudoppalapudi@gmail.com>
2021-04-25 00:57:35 +0530

    Circular Linked List

--- circularlinkedlist.py
+++ circularlinkedlist.py
@@ -7,7 +7,7 @@ class CircularLinkedList:
     def __init__(self):
         self.head = None
 
-    def insertionAtHead(self, data): # adds a new node at the starting of the list
+    def insertionAtHead(self, data):
         newNode = Node(data)
         if self.head is None:
             self.head = newNode
@@ -18,6 +18,7 @@ class CircularLinkedList:
                 if temp.next == self.head:
                     newNode.next = self.head
                     temp.next = newNode
+                    self.head = newNode
                     return
                 temp = temp.next
 
@@ -33,7 +34,27 @@ class CircularLinkedList:
             newNode.next = self.head
             temp.next = newNode
 
-    def deletionAtHead(self): # deletes a node at starting of the list
+    def insertionAtKey(self, data, key):
+        if self.head is None:
+            print("List is Empty")
+        elif self.head.data == key:
+            self.insertionAtHead(data)
+        else:
+            newNode = Node(data)
+            temp = self.head
+            prevNode = None
+            while temp:
+                if temp.data == key:
+                    prevNode.next = newNode
+                    newNode.next = temp
+                    return
+                if temp.next == key:
+                    break
+                prevNode = temp
+                temp = temp.next
+            print("Key Not Found")
+
+    def deletionAtHead(self):
         if self.head is None:
             print("List is empty, nothing to delete")
         elif self.head is self.head.next: # when there is only one node left in the linked list
@@ -60,6 +81,22 @@ class CircularLinkedList:
             prevNode.next = self.head
             temp = None
 
+    def deletionAtKey(self, key):
+        if self.head is None:
+            print("Nothing to delete")
+        elif self.head.data == key:
+            self.deletionAtHead()
+        else:
+            temp = self.head
+            prevNode = self.head
+            while temp.next is not self.head:
+                if temp.data == key:
+                    prevNode.next = temp.next
+                    return
+                prevnode = temp
+                temp = temp.next
+            print("Key Not Found")
+
     def isEmpty(self):
         if self.head is None:
             print("List is Empty")
@@ -77,20 +114,42 @@ class CircularLinkedList:
                     break
                 temp = temp.next
 
-n = CircularLinkedList()
-n.insertionAtHead(1)
-n.insertionAtHead(2)
-n.insertionAtHead(3)
-n.insertionAtHead(4)
-n.insertionAtHead(5)
-n.showList()
-print()
-n.insertionAtEnd(9)
-n.showList()
-print()
-n.deletionAtEnd()
-n.showList()
-print()
-n.deletionAtEnd()
-n.showList()
-
+object = CircularLinkedList()
+while True:
+    print("----------------------------------")
+    print('1 -> Insertion at Head ')
+    print('2 -> Insertion at End ')
+    print('3 -> Insertion at Key ')
+    print('4 -> Deletion at head ')
+    print('5 -> Deletion at End ')
+    print('6 -> Deletion at Key ')
+    print('7 -> Print List ')
+    print('8 -> IsEmpty ')
+    print('Press Any Key to Exit the Program')
+    option = int(input('Enter the option '))
+    if option == 1:
+        data = int(input('Enter the data: '))
+        object.insertionAtHead(data)
+    elif option == 2:
+        data = int(input('Enter the data: '))
+        object.insertionAtEnd(data)
+    elif option == 3:
+        data = int(input('Enter the data: '))
+        key = int(input("Enter the key: "))
+        object.insertionAtKey(data, key)
+    elif option == 4:
+        object.deletionAtHead()
+    elif option == 5:
+        object.deletionAtEnd()
+    elif option == 6:
+        key = int(input("Enter the key: "))
+        object.deletionAtKey(key)
+    elif option == 7:
+        object.showList()
+    elif option == 8:
+        object.isEmpty()
+    else:
+        print('Program Terminated ')
+        print("For more function which perform different operation refer to singly linked linked list program.")
+        print("As those un-included function also have same code for circular linked list i didn't add them here.")
+        break
