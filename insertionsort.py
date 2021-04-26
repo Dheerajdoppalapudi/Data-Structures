eccaacf771bd20b0d23a1f27490a7b1ba6efc3f4
Dheerajdoppalapudi <dheerudoppalapudi@gmail.com>
2021-04-27 01:20:45 +0530

    Insertion Sort

--- insertionsort.py
+++ insertionsort.py
@@ -1,12 +1,11 @@
 def insertionSort(arr):
     for i in range(1, len(arr)):
         temp = arr[i]
-        while i >= 0 and arr[i-1] > temp:
-            arr[i] = arr[i-1]
-            if i == 0: # to avoid negative indexing 
-                break
-            i -= 1
-        arr[i] = temp
+        j = i-1
+        while j >= 0 and arr[j] > temp:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = temp
     return arr
 
 arr = [5,4,10,1,6,2]
