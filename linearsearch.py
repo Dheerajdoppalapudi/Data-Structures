3b1f6c5e5262e7b793998a79f914a47bf62ea50f
Dheerajdoppalapudi <dheerudoppalapudi@gmail.com>
2021-06-30 01:35:06 +0530

    Linear search

---
+++ linearsearch.py
@@ -0,0 +1,14 @@
+#linear search python code
+def linearSearch(arr, element):
+    for i in range(len(arr)):
+        if arr[i] == element:
+            return True
+    return False
+
+string = input("Enter the number ")
+arr = [int(x) for x in string.split()]
+
+if linearSearch(arr,element = int(input('Enter the number to be search '))):
+    print("Number found ")
+else:
+    print("Number not found ")
